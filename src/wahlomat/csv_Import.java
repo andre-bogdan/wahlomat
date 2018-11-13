709ef2e7cecccd87a4fed55f67fe07fa3e54576f
ChWegner <chwegner@pm.me>
2018-11-13 11:24:10 +0100

    Erster

---
+++ src/wahlomat/csv_Import.java
@@ -0,0 +1,25 @@
+package wahlomat;
+
+import java.lang.Object;
+import ;
+
+public class csv_Import {
+
+pString[] row = null;
+String csvFilename = "C:\\work\\sample.csv";
+
+CSVReader csvReader = new CSVReader(new FileReader(csvFilename));
+List content = csvReader.readAll();
+
+for (Object object : content) {
+	row = (String[]) object;
+	
+	System.out.println(row[0]
+	           + " # " + row[1]
+	           + " #  " + row[2]);
+}
+//...
+csvReader.close();
+
+
+}
