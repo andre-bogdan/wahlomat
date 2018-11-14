1a6c5db65dcdc7e939ce16907b7746eec5cfc313
OstSan <manuelosterholzer@web.de>
2018-11-14 15:23:15 +0100

    Auswertung der Wahl + Datei schreiben "FakeNews.txt"

--- src/wahlomat/Wahlomat.java
+++ src/wahlomat/Wahlomat.java
@@ -1,4 +1,11 @@
 package wahlomat;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
 class Staat{
     public String name;
     public int einwohnerzahl, demo,rep,winner;
@@ -7,20 +14,7 @@ class Staat{
         this.name = name;
         this.einwohnerzahl = einwohnerzahl;
     }
-
-    public void setDemo(int demo) {
-        this.demo = demo;
-    }
-
-    public void setRep(int rep) {
-        this.rep = rep;
-    }
-
-    public void setWinner(int winner) {
-        this.winner = winner;
-    }
-
-    
+ 
 }
 
 public class Wahlomat {
@@ -44,16 +38,75 @@ public class Wahlomat {
         for(int i=0;i<staaten.length;i++){
             objectArray[i] = new Staat(staaten[i],einwohner[i]);
         }
+        
+        //Sieger ermiteln/erfaken
         int trump = 0;
+        int zahl_demo;
+        int demo = 0;
+        int rep = 0;
+        String winner = null;
         do{
+            demo = 0;
+            rep = 0;
+            for(int i=0;i<staaten.length;i++){
+                Random stimmen = new Random();
+                zahl_demo = 0;
+                zahl_demo = 1 + stimmen.nextInt(objectArray[i].einwohnerzahl);
+                objectArray[i].demo = zahl_demo;
+                objectArray[i].rep = objectArray[i].einwohnerzahl-zahl_demo;
+                if(objectArray[i].demo>objectArray[i].rep){
+                    objectArray[i].winner = 0;
+                }else{
+                    objectArray[i].winner = 1;
+                }
+            }
             
-            
+            for(int i=0;i<staaten.length;i++){
+                if(objectArray[i].winner==0){
+                    demo=demo+1;
+                }else{
+                    rep=rep+1;
+                }
+            }
+            if(demo>=rep){
+                trump = 0;
+                winner = "Demokraten";
+            }else{
+                trump = 1;
+                winner = "Repuplikaner";
+            }
         }while(trump==0);
         
-        //Funktion SiegerErmitteln aufrufen
-        //Wenn Sieger <> Republikaner -> Funktion SiegerErmitteln aufrufen
         //Ergebnis ausgeben
-        //fertig
+        PrintWriter pWriter = null; 
+        try { 
+            pWriter = new PrintWriter(new BufferedWriter(new FileWriter("FakeNews.txt"))); 
+            for(int i=0;i<staaten.length;i++){
+                pWriter.println("Staat = " + objectArray[i].name);
+                pWriter.println("Einwohner = " + objectArray[i].einwohnerzahl);
+                pWriter.println("Demokraten = " + objectArray[i].demo);
+                pWriter.println("Repuplikaner = " + objectArray[i].rep);
+                pWriter.println("Gewonnen = " + objectArray[i].winner);
+                pWriter.println("____________________________________");
+            }
+            pWriter.println("Tata gewonnen haben man ahnt es kaum die " + winner + "\n\r" + demo + " Gouverneure für die Demokraten\n\r" + rep + " Gouverneure für die Repuplikaner");   
+        } catch (IOException ioe) { 
+            ioe.printStackTrace(); 
+        } finally { 
+            if (pWriter != null){ 
+                pWriter.flush(); 
+                pWriter.close(); 
+            } 
+        } 
+        for(int i=0;i<staaten.length;i++){
+            System.out.println("Staat = " + objectArray[i].name);
+            System.out.println("Einwohner = " + objectArray[i].einwohnerzahl);
+            System.out.println("Demokraten = " + objectArray[i].demo);
+            System.out.println("Repuplikaner = " + objectArray[i].rep);
+            System.out.println("Gewonnen = " + objectArray[i].winner);
+            System.out.println("____________________________________");
+        }
+        System.out.println("Tata gewonnen haben man ahnt es kaum die " + winner + "\n\r" + demo + " Gouverneure für die Demokraten\n\r" + rep + " Gouverneure für die Repuplikaner");
     }
 
 }
